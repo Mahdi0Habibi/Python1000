09ea81958c0731084f0a7d794823aa038ac45387
Bluiteng <bluteng47@gmail.com>
2026-07-12 17:58:28 +0330

    Calculating the surface area and volume of a sphere

---
+++ Introduction/3.py
@@ -0,0 +1,8 @@
+pi = 3.14
+radius = float(input("Please Enter Radius: "))
+
+TotalArea = 4*pi*(radius**2)
+Volume = (4/3)*pi*(radius**3)
+
+print(f"Volume of sphere: {Volume}")
+print(f"Surface Area of sphere: {TotalArea}")
