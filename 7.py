6cbf0bc94e76187ffa5b49a204ddcfa2c706ab09
Bluiteng <bluteng47@gmail.com>
2026-08-22 11:27:39 +0330

    Complex Numbers

---
+++ 7.py
@@ -0,0 +1,5 @@
+real = int(input("Please Enter the Real part: "))
+image = int(input("Please Enter the Image part: "))
+result = complex(real, image)
+#Complex(a,b) function Creates Complex Numbers
+print(result)
