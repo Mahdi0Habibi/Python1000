0609988f95c581b12deb40dace18eb53998ddb3d
Bluiteng <bluteng47@gmail.com>
2026-08-22 11:33:13 +0330

    Molucule Counter

---
+++ 8.py
@@ -0,0 +1,5 @@
+liter = int(input("Please Enter Liter Of Water: "))
+geram = liter * 950
+molucule = 3*pow(10,-23)
+result = geram / molucule
+print(f"{int(result)} Molucules Are in {liter} Liters of Water")
