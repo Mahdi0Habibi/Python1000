b80e9ff526221e789cf7c72b3e66360996997fec
Bluiteng <bluteng47@gmail.com>
2026-08-25 16:21:16 +0330

    Sharpening The Code

--- Introduction/24.py
+++ Introduction/24.py
@@ -1,5 +1,6 @@
 A4 = int(input("Please Enter A4 Price: "))
 pen = int(input("Please Enter Pen Price: "))
 Inflation = float(input("Please Enter Inflation Rate: "))
-Cost = ((pen * 150 * Inflation) / 100) + (A4*50*Inflation/100)
+Inflation /=100
+Cost = ((pen * 150 * Inflation)) + (A4*50*Inflation)
 print(f"Extra Cost is {Cost}")
